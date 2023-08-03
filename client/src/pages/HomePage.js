65c429ccda5c9d03732e1ee1727f952dcffd2556
Brandon Foster <brandon.foster.vt@gmail.com>
2023-08-03 14:23:30 -0400

    display books via tag buttons

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,42 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export const HomePage = () => {
+    const tag = {
+        FRUIT: 'FRUIT',
+        EXPENSIVE: 'EXPENSIVE',
+        FAVORITE: 'FAVORITE',
+    };
+    const tags = [
+        tag.FRUIT, tag.EXPENSIVE, tag.FAVORITE,
+    ];
+    const books = [
+        {
+            title: 'Apple Book',
+            tagList: [
+                tag.FRUIT,
+            ],
+        },
+        {
+            title: 'Banana Book',
+            tagList: [
+                tag.FRUIT,
+                tag.EXPENSIVE,
+                tag.FAVORITE,
+            ],
+        },
+        {
+            title: 'Cherry Book',
+            tagList: [
+                tag.FRUIT,
+                tag.EXPENSIVE,
+            ],
+        },
+    ];
+    const [bookListState, setBookListState] = useState([]);
+    function handleTagButtonClick(e, tag) {
+        const newList = books.filter(b => b.tagList.includes(tag));
+        setBookListState([...newList]);
+    }
     return (
         <>
             <h1>Brandon Foster</h1>
@@ -13,6 +49,16 @@ export const HomePage = () => {
                 <a href="#" className="btn btn-lg btn-secondary fw-bold border-white bg-white">utilities</a>
                 <a href="#" className="btn btn-lg btn-secondary fw-bold border-white bg-white">resume</a>
             </p>
+            {tags.map((t, i) => {
+                return (
+                    <button key={`tagButton${i}`} type='button' className='btn btn-secondary' onClick={e => handleTagButtonClick(e, t)}>{t}</button>
+                );
+            })}
+            {bookListState.map((b, i) => {
+                return (
+                    <p key={`book${i}`}>{ b.title }</p>
+                );
+            })}
         </>
     );
 };
