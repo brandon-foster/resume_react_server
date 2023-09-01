9ea809f8932c7463492543292cd49101a2c8edc1
Brandon Foster <brandon.foster.vt@gmail.com>
2023-09-01 11:58:56 -0400

    terminal icon; change home to portfolio; change Portfolio link to Projects link

--- client/src/layout/Layout.js
+++ client/src/layout/Layout.js
@@ -1,14 +1,20 @@
 import { Outlet, Link } from "react-router-dom";
 
+import { IconTerminal } from '../icon-html/IconTerminal';
+
 export const Layout = () => {
   return (
     <div className="shadow-container h-100 w-100">
       <div className="d-flex w-100 h-100 p-3 mx-auto flex-column">
         <header className="w-max-42em d-flex w-100 text-center mx-auto mb-auto flex-column">
           <div>
-            <h3 className="float-md-start mb-0">home</h3>
+              <a href='/' className='text-decoration-none text-white'>
+            <h3 className='float-md-start mb-0'>
+                <IconTerminal />&nbsp;portfolio
+            </h3>
+              </a>
             <nav className="nav nav-masthead justify-content-center float-md-end">
-              <Link to='/' className='nav-link fw-bold py-1 px-0'>Portfolio</Link>
+              <Link to='/' className='nav-link fw-bold py-1 px-0'>Projects</Link>
               <Link to='/resume' className='nav-link fw-bold py-1 px-0'>Resume</Link>
               <Link to='/philosophy' className='nav-link fw-bold py-1 px-0'>Philosophy</Link>
             </nav>
