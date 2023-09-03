abd2c54335ec7ced670a46e704e5f845618a2509
Brandon Foster <brandon.foster.vt@gmail.com>
2023-09-03 13:53:12 -0400

    quoteList is moved to separate file

--- client/src/pages/PhilosophyPage.js
+++ client/src/pages/PhilosophyPage.js
@@ -1,15 +1,8 @@
 import { useState } from 'react';
 
+import { quoteList } from './philosophy/quoteList.js';
+
 export const PhilosophyPage = () => {
-  const quoteList = [
-    `Hope is not an engineering best practice. - Team Lead, circa 2017, paraphrased`,
-    `You only need to test the boundary. Then you can refactor the lower layers. - L.H., circa 2022, paraphrased`,
-    `Before your fingers touch the keyboard, know what you want to do. - C.S. professor during a college visit, circa 2011, paraphrased`,
-    `Nothing is difficult. It depends on how much time you have. - F.L., circa 2011, paraphrased`,
-    `I see what's going on. They did entirely too much pre-factoring. - P.G., circa 2021, paraphrased`,
-    `I know I did a good job refactoring when I end up fewer lines than I did before. - R.G., circa 2022, paraphrased`,
-    `Nobody has yet to answer me. Why are we adding jars to git? - K.K., circa 2023, paraphrased`,
-  ];
   const availIndexesList = provideAvailableIndexesOf(quoteList);
   const [availIndexes, setAvailIndexes] = useState([...availIndexesList]);
   const [quote, setQuote] = useState(() => {
---
+++ client/src/pages/philosophy/quoteList.js
@@ -0,0 +1,8 @@
+export const quoteList = [
+  `Hope is not an engineering best practice. - Team Lead, circa 2017, paraphrased`,
+  `You only need to test the boundary. Then you can refactor the lower layers. - L.H., circa 2022, paraphrased`,
+  `Before your fingers touch the keyboard, know what you want to do. - C.S. professor during a college visit, circa 2011, paraphrased`,
+  `Nothing is difficult. It depends on how much time you have. - F.L., circa 2011, paraphrased`,
+  `I see what's going on. They did entirely way too much pre-factoring. - P.G., circa 2021, paraphrased`,
+  `I know I did a good job refactoring when I end up fewer lines than I did before. - R.G., circa 2022, paraphrased`,
+];
