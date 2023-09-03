1ff4a60bfcd7a017fe59f73d361e3f48d8a95606
Brandon Foster <brandon.foster.vt@gmail.com>
2023-09-03 16:23:28 -0400

    edit Philosphy strings; use repeat icon

--- client/src/data/strings.js
+++ client/src/data/strings.js
@@ -1,10 +1,9 @@
 export const philosophyStrings = {
   h1: 'Philosophy',
-  quoteButton: 'Next Quotation',
   hopeIsNot: `Hope is not an engineering best practice. - K.L., circa 2017, paraphrased`,
-  testTheBoundary: `You only need to test the boundary. Then you can refactor the lower layers. - L.H., circa 2022, paraphrased`,
+  testTheBoundary: `You only need to test the boundary. Then you can refactor. - L.H., circa 2022, paraphrased`,
   beforeFingersTouchKeyboard: `Before your fingers touch the keyboard, know what you want to do. - C.S. professor during a college visit, circa 2011, paraphrased`,
-  nothingIsDifficult: `Nothing is difficult. It depends on how much time you have. - F.L., circa 2011, paraphrased`,
+  nothingIsDifficult: `Nothing is difficult. It just depends on how much time you have. - F.L., circa 2011, paraphrased`,
   prefactoring: `I see what's going on. They did entirely way too much pre-factoring. - P.G., circa 2021, paraphrased`,
   fewerLinesThanBefore: `I know I did a good job refactoring when I end up fewer lines than I did before. - R.G., circa 2022, paraphrased`,
 };
--- client/src/pages/PhilosophyPage.js
+++ client/src/pages/PhilosophyPage.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import { quoteList } from './philosophy/quoteList';
 import { philosophyStrings } from '../data/strings';
+import { IconRepeat } from '../icon-html/IconRepeat';
 
 export const PhilosophyPage = () => {
   const availIndexesList = provideAvailableIndexesOf(quoteList);
@@ -36,8 +37,8 @@ export const PhilosophyPage = () => {
         <div className='col-lg-6 mx-auto'>
           <p className='lead mb-4'>{quote}</p>
           <div className='d-grid gap-2 d-sm-flex justify-content-sm-center'>
-            <button type='button' className='btn btn-primary btn-lg px-4 gap-3'
-              onClick={() => setQuote(provideQuote(provideRandomAndRemoveFrom(availIndexes)))}>{philosophyStrings.quoteButton}</button>
+            <button type='button' className='btn btn-primary btn-lg px-4 gap-3 w-50'
+              onClick={() => setQuote(provideQuote(provideRandomAndRemoveFrom(availIndexes)))}>{<IconRepeat></IconRepeat>}</button>
             {/* <button type='button' className='btn btn-outline-secondary btn-lg px-4'>Secondary</button> */}
           </div>
         </div>
