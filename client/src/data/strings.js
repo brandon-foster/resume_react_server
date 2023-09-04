3547ce57f0ebc4ac077abac4462e311e95c6c22b
Brandon Foster <brandon.foster.vt@gmail.com>
2023-09-04 14:35:48 -0400

    remove hopeIsNot quotation, update footer

--- client/src/data/strings.js
+++ client/src/data/strings.js
@@ -4,7 +4,6 @@ export const homeStrings = {
 
 export const philosophyStrings = {
   h1: 'Philosophy',
-  hopeIsNot: `Hope is not an engineering best practice. - K.L., circa 2017, paraphrased`,
   testTheBoundary: `You only need to test the boundary. Then you can refactor. - L.H., circa 2022, paraphrased`,
   beforeFingersTouchKeyboard: `Before your fingers touch the keyboard, know what you want to do. - C.S. professor during a college visit, circa 2011, paraphrased`,
   nothingIsDifficult: `Nothing is difficult. It just depends on how much time you have. - F.L., circa 2011, paraphrased`,
@@ -13,6 +12,8 @@ export const philosophyStrings = {
 };
 
 export const layoutStrings = {
-  footer: `Made with &#10024;
-    <br />&copy; 2023 No rights reserved. <a href='https://bitbucket.org/brandon-foster/resume_react_server/src/master/' target='_blank' rel='noreferrer'>Here's the source.</a>`.trim(),
+  footer: `
+    Made with &#10024;
+    &copy; ${new Date().getFullYear()}
+    `.trim(),
 };
