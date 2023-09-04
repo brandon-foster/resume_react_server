843c0e243ec8539a6bb7ba5c02b45c2bbc915fdc
Brandon Foster <brandon.foster.vt@gmail.com>
2023-09-04 16:28:22 -0400

    resume has mt-5

--- client/src/pages/ResumePage.js
+++ client/src/pages/ResumePage.js
@@ -54,7 +54,7 @@ const WorkExperience = ({ info }) => {
 export const ResumePage = () => {
   return (
     <>
-      <h1 className='display-2'>Experience</h1>
+      <h1 className='display-2 mt-5'>Experience</h1>
       <WorkExperience info={employers.cgi} />
       <WorkExperience info={employers.boeing} />
       {/* <WorkExperience info={provideInfo('boeing')}/> */}
